fc499b050583e9ef3f377f82f0222265403613f9
ilja <the.ilja@gmail.com>
2022-11-04 11:29:44 +0300

    start screen

--- app/src/main/java/com/a530games/jackal/screens/LoadingScreen.java
+++ app/src/main/java/com/a530games/jackal/screens/LoadingScreen.java
@@ -1,13 +1,18 @@
 package com.a530games.jackal.screens;
 
 import android.graphics.Color;
+import android.graphics.Paint;
 
 import com.a530games.framework.Graphics;
 import com.a530games.framework.Screen;
+import com.a530games.framework.helpers.cor.Handler;
+import com.a530games.framework.helpers.cor.Step;
 import com.a530games.jackal.Assets;
 import com.a530games.jackal.Jackal;
 import com.a530games.jackal.levels.ArenaLevel;
 import com.a530games.jackal.levels.Level;
+
+import java.util.function.Function;
 
 public class LoadingScreen extends Screen
 {
@@ -15,31 +20,42 @@ public class LoadingScreen extends Screen
 
     String loadingLevelCode;
 
+    public float titlePosition = 200;
+
+    Paint titlePaint;
+
+    private boolean part1Loaded = false;
+    private boolean part2Loaded = false;
+    private boolean part3Loaded = false;
+
     public LoadingScreen(Jackal game, String levelCode)
     {
         super(game);
 
         this.loadingLevelCode = levelCode;
 
-        // todo: load only this this map sprite
-        Assets.bg = this.game.getGraphics().newPixmap("images/bg.jpg", Graphics.PixmapFormat.RGB565);
+        this.titlePaint = new Paint();
+        this.titlePaint.setColor(Color.WHITE);
+        this.titlePaint.setTextAlign(Paint.Align.CENTER);
+        this.titlePaint.setTextSize(100);
     }
 
     @Override
     public void update(float deltaTime)
     {
-        // пропускаем пару секунд для промо видел
+        this.titlePosition += 2 * deltaTime;
+        // if (this.titlePosition > 200) this.titlePosition = 200;
 
-        if (this.timer > 1) {
-            Assets.loadPart1(this.game.getGraphics(), this.game.getAudio());
+        if (this.timer > 1 && !this.part1Loaded) {
+            this.loadingPart1();
         }
 
-        if (this.timer > 2) {
-            Assets.loadPart2(this.game.getGraphics(), this.game.getAudio());
+        if (this.timer > 2 && !this.part2Loaded) {
+            this.loadingPart2();
         }
 
-        if (this.timer > 3) {
-            Assets.loadPart3(this.game.getGraphics(), this.game.getAudio());
+        if (this.timer > 3 && !this.part3Loaded) {
+            this.loadingPart3();
         }
 
         if (this.timer > 5) {
@@ -54,9 +70,13 @@ public class LoadingScreen extends Screen
     @Override
     public void present(float deltaTime)
     {
-        this.game.getGraphics().drawLine(0, 0, this.game.getGraphics().getFrameBufferWidth(), this.game.getGraphics().getFrameBufferHeight(), Color.YELLOW);
-        this.game.getGraphics().drawText("530 games", 100, 150, 100, Color.MAGENTA);
-        this.game.getGraphics().drawPixmap(Assets.bg, 10, 10);
+        Graphics g = this.game.getGraphics();
+
+        // g.clear(Color.BLACK);
+
+        int screenCenterX = (int) Math.ceil(g.getFrameBufferWidth() * 0.5);
+        g.drawText("530 games", screenCenterX, (int) this.titlePosition, this.titlePaint);
+        // this.game.getGraphics().drawPixmap(Assets.bg, 10, 10);
     }
 
     @Override
@@ -72,5 +92,23 @@ public class LoadingScreen extends Screen
     @Override
     public void dispose() {
 
+    }
+
+    private void loadingPart1 ()
+    {
+        Assets.loadPart1(this.game.getGraphics(), this.game.getAudio());
+        this.part1Loaded = true;
+    }
+
+    private void loadingPart2 ()
+    {
+        Assets.loadPart2(this.game.getGraphics(), this.game.getAudio());
+        this.part2Loaded = true;
+    }
+
+    private void loadingPart3 ()
+    {
+        Assets.loadPart3(this.game.getGraphics(), this.game.getAudio());
+        this.part3Loaded = true;
     }
 }
